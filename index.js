7bc2c5ec2506ae8a2158a3fa1d6bf4490c75ab3d
ShadowTheBest <101932050+ShadowTheBest@users.noreply.github.com>
2022-06-30 10:17:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ bot.on("message", async message => {
     
         let args = message.content.trim().split(/ +/g)
         
-        utilisateur = message.mentions.members.first();
+        utilisateur = message.mentions.users.first();
         temps = args[2]
         raison = args[3]
         
